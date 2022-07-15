c095a0374fee21dc4137e1cfe14667348b2be210
rfajri27 <rfajri255@gmail.com>
2022-07-15 11:06:15 +0700

    update starter code & solution for latihan-model-serving-flask

--- latihan_model_serving_flask/solution/main.py
+++ latihan_model_serving_flask/solution/main.py
@@ -33,4 +33,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
--- latihan_model_serving_flask/starter/main.py
+++ latihan_model_serving_flask/starter/main.py
@@ -2,4 +2,18 @@ import json
 import tensorflow as tf
 from flask import Flask, request
 
+
+#================Membuat Web App Sederhana Menggunakan Flask========================
 # Your code here
+#===================================================================================
+
+
+#============Membuat API Endpoint dalam Web App sebagai Model Serving===============
+# Your code here
+#===================================================================================
+
+
+
+#=============================Menjalankan Web App===================================
+# Your code here
+#===================================================================================
