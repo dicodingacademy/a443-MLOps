3eac3c4bf0c6a82a5d764dae57d75cacc4bdc8e2
rfajri27 <rfajri255@gmail.com>
2022-08-05 15:31:43 +0700

    update

--- latihan_python_clean_code/solution/eda_library.py
+++ latihan_python_clean_code/solution/eda_library.py
@@ -1,27 +1,19 @@
-"""
-Author: Dicoding
-Date: 5/8/2022
-This is the eda_library.py module.
-Usage:
-- EDA
-"""
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style='darkgrid')
 
-def import_data(pth):
-    """Returns a pandas DataFrame for the CSV found at pth
+def load_data(path):
+    """Returns a pandas DataFrame for the CSV found at path
 
     Args:
-        pth (str): a path to the CSV file
+        path (str): a path to the CSV file
 
     Returns:
         pandas DataFrame
     """
-    return pd.read_csv(pth)
+    return pd.read_csv(path)
 
 
 def perform_eda(dataframe):
@@ -38,13 +30,24 @@ def perform_eda(dataframe):
     eda_df = eda_df[eda_df['TotalCharges'] != ' ']
     eda_df['TotalCharges'] = eda_df.TotalCharges.astype(float)
     
+    categorical_columns = [
+    'gender',
+    'SeniorCitizen',
+    'Partner',
+    'StreamingTV',
+    'PhoneService',
+    'InternetService',
+    'PaperlessBilling',
+    'Churn'
+    ]
+    numerical_columns = ['MonthlyCharges', 'TotalCharges', 'tenure']
+    
     churn = eda_df.Churn.value_counts()
     plt.figure(figsize=(7, 7))
     plt.pie(churn, labels=['No', 'Yes'], autopct='%1.1f%%')
-    plt.title('Persentasi Churn Customer', loc='center', fontsize=18)
+    plt.title('Presentasi Churn Customer', loc='center', fontsize=18)
     plt.savefig('images/persentasi_churn.png')
     
-    numerical_columns = ['MonthlyCharges', 'TotalCharges', 'tenure']
     fig, ax = plt.subplots(1, 3, figsize=(14, 7))
     eda_df[eda_df.Churn == 'No'][numerical_columns].hist(
         bins=20,
@@ -61,14 +64,10 @@ def perform_eda(dataframe):
     plt.tight_layout()
     plt.savefig('images/distribusi_kolom_numerik.png')
     
-    fig, ax = plt.subplots(3, 3, figsize=(14, 12))
-    sns.countplot(data=eda_df, x='gender', hue='Churn', ax=ax[0][0])
-    sns.countplot(data=eda_df, x='Partner', hue='Churn', ax=ax[0][1])
-    sns.countplot(data=eda_df, x='SeniorCitizen', hue='Churn', ax=ax[0][2])
-    sns.countplot(data=eda_df, x='PhoneService', hue='Churn', ax=ax[1][0])
-    sns.countplot(data=eda_df, x='StreamingTV', hue='Churn', ax=ax[1][1])
-    sns.countplot(data=eda_df, x='InternetService', hue='Churn', ax=ax[1][2])
-    sns.countplot(data=eda_df, x='PaperlessBilling', hue='Churn', ax=ax[2][1])
+    fig, ax = plt.subplots(len(categorical_columns), 1, figsize=(10, 35))
+
+    for idx, column in enumerate(categorical_columns):
+        sns.countplot(data=eda_df, x=column, hue='Churn', ax=ax[idx])
     plt.tight_layout()
     plt.savefig('images/distribusi_kolom_kategorik.png')
     
@@ -76,6 +75,6 @@ def perform_eda(dataframe):
 
 
 if __name__ == '__main__':
-    customer_df = import_data(pth='./data/Telco-Customer-Churn.csv')
+    customer_df = load_data(path='./data/Telco-Customer-Churn.csv')
     
     eda_df = perform_eda(dataframe=customer_df)
